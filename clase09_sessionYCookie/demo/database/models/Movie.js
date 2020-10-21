6799587118891b1e696034053285ca70dcba917f
alejandrodh <alejandro@digitalhouse.com>
2020-10-21 11:16:46 -0300

    proyecto base para session y cookie.

---
+++ clase09_sessionYCookie/demo/database/models/Movie.js
@@ -0,0 +1,50 @@
+module.exports = function (sequelize, dataTypes){
+
+    let alias = 'Movie'; //Este alias se busca como nombre en de la tabla en plural dentro de la base de datos.
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        created_at : {
+            type: dataTypes.DATE,
+            allowNull:true,
+        },
+        updated_at: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        },
+        title: {
+            type: dataTypes.STRING
+        },
+        rating: {
+            type: dataTypes.DECIMAL
+        },
+        awards: {
+            type: dataTypes.INTEGER
+        },
+        release_date: {
+            type: dataTypes.DATE
+        },
+        length: {
+            type: dataTypes.INTEGER
+        },
+        genre_id: {
+            type: dataTypes.INTEGER
+        },
+
+    }
+
+    let config = {
+        // tableName : "movies"
+        timestamps:false, //Aclareción en caso de no explicitar created_at, deleted_at y updated_at
+        // underscored: true, //Aclareción en caso que los timestamps usen guiones bajos en lugar de camelCase.
+    };
+
+    const Movie = sequelize.define(alias, cols, config);
+
+    return Movie;
+
+}
---
+++ clase09_sessionYCookie/demo/readme.md
@@ -0,0 +1,11 @@
+Vimos rutas por `GET` y rutas por `POST`.  
+En el controlador de pelícualas creamos los métodos:  
+    - add  
+    - store  
+    - edit  
+    - update  
+    - destroy  
+Usamos los modelos y `sequelize` para aplicar los métodos:  
+    - create  
+    - update  
+    - destroy  
