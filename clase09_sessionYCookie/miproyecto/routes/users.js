5472f7f8dce1ce3c8134e888adcdb382c642d2a6
alejandrodh <alejandro@digitalhouse.com>
2020-11-02 10:36:07 -0300

    logout

--- clase09_sessionYCookie/miproyecto/routes/users.js
+++ clase09_sessionYCookie/miproyecto/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var usersController = require('../controllers/usersController')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/logout', usersController.logout);
 
 module.exports = router;
