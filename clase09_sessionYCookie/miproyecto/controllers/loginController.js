e3dcceb871652ebc9137243070f7e7f9f69f00bb
alejandrodh <alejandro@digitalhouse.com>
2021-05-28 18:38:17 -0300

    login y registro con validaciones y control de acceso

--- clase09_sessionYCookie/miproyecto/controllers/loginController.js
+++ clase09_sessionYCookie/miproyecto/controllers/loginController.js
@@ -15,27 +15,29 @@ let loginController = {
     },
     //login con validaciones para la siguiente clase   
     login: function(req, res){
-       //1 encontrar el email
-       //2 Chequear que la contraseña coincida.
+         //1 Creamos una varable para guardar los errores del form
+        let errors = {};
+        
+        //2 encontrar el user con el mail
        users.findOne({
            where: [{ email : req.body.email }]
        })
         .then( function(user){
-            let errors = {};
-            //El email no está en la base de datos
+            
+            //2.1 El email no está en la base de datos
             if(user == null){
                 errors.login = "Email incorrecto";
                 res.locals.errors = errors;
                 
                 return res.render('login');
             } else if (bcrypt.compareSync(req.body.password, user.password) == false ){
-                //EL email existe pero la contraseña está mal
+                //2.2 EL email existe pero la contraseña está mal
                 errors.login = "Contraseña equivocada";
                 res.locals.errors = errors;
                 
                 return res.render('login');
             } else {
-                //El email existe y coinciden las contraseñas
+                //2.3 El email existe y coinciden las contraseñas
                 req.session.user = user;
 
                 if(req.body.rememberme != undefined){
